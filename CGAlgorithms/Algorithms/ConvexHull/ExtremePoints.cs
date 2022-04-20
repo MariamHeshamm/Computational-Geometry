689b763a30c12ce5020fafaf2f793d8d16d93f7a
Mariam Hesham <49057961+MariamHeshamm@users.noreply.github.com>
2022-04-20 05:19:55 +0200

    Finished Extreme Points

--- CGAlgorithms/Algorithms/ConvexHull/ExtremePoints.cs
+++ CGAlgorithms/Algorithms/ConvexHull/ExtremePoints.cs
@@ -11,7 +11,40 @@ namespace CGAlgorithms.Algorithms.ConvexHull
     {
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
+            if (points.Count == 1 || points.Count == 2 || points.Count == 3)//case1 and case 2
+            {
+                outPoints = points;
+                return;
+            }
 
+            Enums.PointInPolygon ok = 0;
+             for (int i = 0; i < points.Count(); i++) 
+            {
+                for (int j = 0; j < points.Count; j++) 
+                {
+                    for (int k = 0; k < points.Count; k++) 
+                    {
+                        if (i == points.Count)
+                            i--;
+                        for (int l = 0; l < points.Count; l++)
+                        {
+                            if (j == points.Count)
+                                j--;
+                            if (k == points.Count)
+                                k--;
+                            if (points[l] != points[i] && points[l] != points[j] && points[l] != points[k])
+                            {
+                                 ok = HelperMethods.PointInTriangle(points[l], points[i], points[j], points[k]);
+                                if (ok == Enums.PointInPolygon.Inside || ok == Enums.PointInPolygon.OnEdge)
+                                {
+                                    points.RemoveAt(l);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        outPoints = points;
         }
 
         public override string ToString()
